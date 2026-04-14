d872f68d63639775ad532eec3a2e31c77a300a98
sranderca <anderson.calderon@correounivalle.edu.com>
2026-04-14 15:20:56 -0500

    quitando variable de backend en imagenes destacadas

--- frontend-ecommerce/components/featured-products.tsx
+++ frontend-ecommerce/components/featured-products.tsx
@@ -47,10 +47,7 @@ const FeaturedProducts = () => {
                   <div className="p-1">
                     <Card className="py-4 border border-gray-200 shadow-none">
                       <CardContent className="relative flex items-center justify-center px-6 py-2">
-                        <img
-                          src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${imageUrl}`}
-                          alt={productName}
-                        />
+                        <img src={imageUrl} alt={productName} />
                         <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
                           <div className="flex justify-center gap-x-6">
                             <IconButton
